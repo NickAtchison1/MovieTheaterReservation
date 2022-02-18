86c320ab7ff7f7da1fece8686834c54adcfb83b7
Nick Atchison <n.atchison55@gmail.com>
2022-02-18 09:37:00 -0500

    Updated MovieShowing display models - removed MovieShowing List Item for now -Might be better to show a list of times in the Movie list item, but can create a new file if not

--- MovieTheaterReservation/Shared/DisplayModels/MovieShowing/MovieShowingCreate.cs
+++ MovieTheaterReservation/Shared/DisplayModels/MovieShowing/MovieShowingCreate.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MovieTheaterReservation.Shared.DisplayModels.MovieShowing
 {
-    internal class MovieShowingCreate
+    public class MovieShowingCreate
     {
+        public int MovieId { get; set; }
+        public int AuditoriumId { get; set; }
+        public DateTime MovieShowingDate { get; set; }
+        public TimeSpan MovieShowingTime { get; set; }
     }
 }
